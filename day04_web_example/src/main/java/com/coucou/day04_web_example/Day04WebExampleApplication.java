46ab75b7ca27ecc84ac0345d1c74289210607c3a
coucou <1131088589@qq.com>
2020-04-04 23:28:23 +0800

    4.4

---
+++ day04_web_example/src/main/java/com/coucou/day04_web_example/Day04WebExampleApplication.java
@@ -0,0 +1,13 @@
+package com.coucou.day04_web_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day04WebExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day04WebExampleApplication.class, args);
+    }
+
+}
---
+++ day04_web_example/src/test/java/com/coucou/day04_web_example/Day04WebExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coucou.day04_web_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day04WebExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
